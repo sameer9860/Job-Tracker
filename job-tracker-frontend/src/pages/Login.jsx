8a70f1e0adedb03d3de46fbc6929992abb0f953b
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-01-22 13:10:57 +0545

    Added Api Dashboard and Login Files

---
+++ job-tracker-frontend/src/pages/Login.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import API from "../services/api";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await API.post("token/", {
+        username,
+        password,
+      });
+
+      localStorage.setItem("access_token", res.data.access);
+      localStorage.setItem("refresh_token", res.data.refresh);
+
+      window.location.href = "/dashboard";
+    } catch (err) {
+      setError("Invalid credentials");
+    }
+  };
+
+  return (
+    <div style={{ maxWidth: "400px", margin: "100px auto" }}>
+      <h2>Login</h2>
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <br /><br />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br /><br />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
